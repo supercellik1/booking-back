d7ebe7741d67b2b1d424707f5fb11741d6942880
AIVdarksunVIA <annasimvol606@gmail.com>
2026-04-15 20:01:23 +0300

    forgot about this

---
+++ Backend/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
