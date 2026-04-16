60ea3b172a9936fc41a0f9ca21a86b7fb66b36fd
AIVdarksunVIA <annasimvol606@gmail.com>
2026-04-16 15:41:02 +0300

    one line

--- Backend/Models/AppDbContext.cs
+++ Backend/Models/AppDbContext.cs
@@ -7,4 +7,5 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<User> Users => Set<User>();
     public DbSet<Hotel> Hotels => Set<Hotel>();
+    public DbSet<Booking> Bookings { get; set; }
 }
