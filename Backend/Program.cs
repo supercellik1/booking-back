fae47055407bd3839cf415426407ad5f3d9f453f
AIVdarksunVIA <annasimvol606@gmail.com>
2026-04-16 15:59:50 +0300

    t

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -13,9 +13,6 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddScoped<TokenService>();
 
 builder.Services.AddCors(options =>
